cb834f16576af9c38340087203b4de3a15e08b3b
WarEagle451 <56392435+WarEagle451@users.noreply.github.com>
2022-06-21 16:38:31 -0400

    Update assert.hpp

--- Mythos/src/mythos/core/assert.hpp
+++ Mythos/src/mythos/core/assert.hpp
@@ -1,11 +1,7 @@
 #pragma once
-#include <mythos/core/log.hpp> // MYL_CORE_FATAL()
-
-#include <filesystem>
-
 #ifdef MYL_BUILD_DEBUG
 #	define MYL_ENABLE_ASSERTS
-#	if _MSC_VER
+#	if MYL_COMPILER_MSVC
 #		define MYL_DEBUGBREAK() __debugbreak()
 #	else
 #		define MYL_DEBUGBREAK() __builtin_trap()
@@ -14,16 +10,34 @@
 #	define MYL_DEBUGBREAK()
 #endif
 
-#ifdef MYL_ENABLE_ASSERTS
-#	define MYL_ASSERT_IMPL(output, check, msg, ...) { if(!(check)) { output(msg, __VA_ARGS__); MYL_DEBUGBREAK(); } }
+#define MYL_MAKE_ASSERT(condition, output, msg, ...) {\
+	if (!(condition)) MYL_UNLIKELY {\
+		output(msg, __VA_ARGS__);\
+		MYL_DEBUGBREAK();\
+	}\
+}
 
-#	define MYL_INTERNAL_ASSERT_MSG(check, ...) MYL_ASSERT_IMPL(MYL_CORE_FATAL, check, "Assertion failed: {0}", __VA_ARGS__)
-#	define MYL_INTERNAL_ASSERT_NO_MSG(check) MYL_ASSERT_IMPL(MYL_CORE_FATAL, check, "Assertion '{0}' failed at {1}:{2}", #check, std::filesystem::path(__FILE__).filename().string(), __LINE__)
+#define MYL_MAKE_VERIFY(condition, exception, ...) {\
+    if (!(condition)) MYL_UNLIKELY\
+        throw exception(__VA_ARGS__);\
+}
 
-#	define MYL_INTERNAL_ASSERT_GET_MACRO_NAME(check, arg, macro, ...) macro
-#	define MYL_INTERNAL_ASSERT_GET_MACRO(...) MYL_EXPAND_MACRO(MYL_INTERNAL_ASSERT_GET_MACRO_NAME(__VA_ARGS__, MYL_INTERNAL_ASSERT_MSG, MYL_INTERNAL_ASSERT_NO_MSG))
+#ifndef MYL_DISABLE_ASSERTS /// MYTodo: Use source location
+#	include "except.hpp"
+#	define MYL_CORE_VERIFY(condition, ...) MYL_MAKE_VERIFY(condition, myl::core_exception, __VA_ARGS__)
+#	if defined(MYL_ENABLE_ASSERTS) || defined(MYL_BUILD_DEBUG)
+#		include "log.hpp"
 
-#	define MYL_CORE_ASSERT(...) MYL_EXPAND_MACRO(MYL_INTERNAL_ASSERT_GET_MACRO(__VA_ARGS__)(__VA_ARGS__))
+#		include <filesystem>
+
+#		define MYL_INTERNAL_ASSERT_MSG(check, ...) MYL_MAKE_ASSERT(check, MYL_CORE_FATAL, "Assertion failed: {0}", __VA_ARGS__)
+#		define MYL_INTERNAL_ASSERT_NO_MSG(check) MYL_MAKE_ASSERT(check, MYL_CORE_FATAL, "Assertion '{0}' failed at {1}:{2}", #check, std::filesystem::path(__FILE__).filename().string(), __LINE__)
+#		define MYL_INTERNAL_ASSERT_GET_MACRO_NAME(check, arg, macro, ...) macro
+#		define MYL_INTERNAL_ASSERT_GET_MACRO(...) MYL_EXPAND_MACRO(MYL_INTERNAL_ASSERT_GET_MACRO_NAME(__VA_ARGS__, MYL_INTERNAL_ASSERT_MSG, MYL_INTERNAL_ASSERT_NO_MSG))
+
+	/// MYTodo: Find a way to allow to pass thorugh args and also use it as a formater
+#		define MYL_CORE_ASSERT(...) MYL_EXPAND_MACRO(MYL_INTERNAL_ASSERT_GET_MACRO(__VA_ARGS__)(__VA_ARGS__))
+#	endif
 #else
-#	define MYL_CORE_ASSERT(...)
+#	define MYL_CORE_VERIFY(condition, ...)
 #endif
