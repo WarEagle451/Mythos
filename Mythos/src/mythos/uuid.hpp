e8eb7b355966feee8b73d2b81b12ae419c2ab094
WarEagle451 <56392435+WarEagle451@users.noreply.github.com>
2022-07-10 21:05:07 -0400

    Update uuid.hpp

--- Mythos/src/mythos/uuid.hpp
+++ Mythos/src/mythos/uuid.hpp
@@ -3,8 +3,6 @@
 
 #include <string>
 
-/// MYTodo: Add ways to get signifigate data from the uuid types, ie low time, mac address
-
 namespace myl {
 	enum class uuid_version {
 		unknown		= 0x00,
@@ -23,27 +21,27 @@ namespace myl {
 		reserved
 	};
 
-	class uuid { /// MYTodo: can do clas MYL_API uuid instead of all inside?
+	class MYL_API uuid {
 		u8 m_bytes[16]{}; // 128 bits
 	public:
-		MYL_API MYL_NO_DISCARD constexpr uuid() noexcept;
-		MYL_API MYL_NO_DISCARD uuid(uuid_version);
-		MYL_API MYL_NO_DISCARD constexpr uuid(u8*);
-		MYL_API MYL_NO_DISCARD constexpr uuid(std::string_view);
+		constexpr uuid() noexcept;
+		uuid(uuid_version);
+		constexpr uuid(u8*);
+		constexpr uuid(std::string_view);
 
-		MYL_API MYL_NO_DISCARD constexpr u8* data();
-		MYL_API MYL_NO_DISCARD constexpr const u8* data() const;
+		MYL_NO_DISCARD constexpr u8* data();
+		MYL_NO_DISCARD constexpr const u8* data() const;
 
-		MYL_API MYL_NO_DISCARD bool nil() const;
-		MYL_API MYL_NO_DISCARD constexpr uuid_version version() const;
-		MYL_API MYL_NO_DISCARD constexpr uuid_variant variant() const;
-		MYL_API MYL_NO_DISCARD constexpr std::string string(bool a_brackets = false, bool a_dashed = true) const;
+		MYL_NO_DISCARD bool nil() const;
+		MYL_NO_DISCARD constexpr uuid_version version() const;
+		MYL_NO_DISCARD constexpr uuid_variant variant() const;
+		MYL_NO_DISCARD constexpr std::string string(bool a_brackets = false, bool a_dashed = true) const;
 
-		MYL_API MYL_NO_DISCARD bool operator==(const uuid& rhs) const;
-		MYL_API MYL_NO_DISCARD bool operator<(const uuid& rhs) const;
-		MYL_API MYL_NO_DISCARD bool operator>(const uuid& rhs) const;
-		MYL_API MYL_NO_DISCARD bool operator<=(const uuid& rhs) const { return !(*this > rhs); }
-		MYL_API MYL_NO_DISCARD bool operator>=(const uuid& rhs) const { return !(*this < rhs); }
+		MYL_NO_DISCARD bool operator==(const uuid& rhs) const;
+		MYL_NO_DISCARD bool operator<(const uuid& rhs) const;
+		MYL_NO_DISCARD bool operator>(const uuid& rhs) const;
+		MYL_NO_DISCARD bool operator<=(const uuid& rhs) const { return !(*this > rhs); }
+		MYL_NO_DISCARD bool operator>=(const uuid& rhs) const { return !(*this < rhs); }
 	};
 }
 
