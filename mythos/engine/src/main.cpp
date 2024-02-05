d66858f50815f535d668f8d72e7cdbc24bd7cd92
WarEagle451 <56392435+WarEagle451@users.noreply.github.com>
2024-02-05 16:36:17 -0500

    Update main.cpp

--- mythos/engine/src/main.cpp
+++ mythos/engine/src/main.cpp
@@ -5,7 +5,7 @@
 auto main(MYL_MAYBE_UNUSED int arg_count, MYL_MAYBE_UNUSED char** args) -> int {
     myth::log::init();
     MYTHOS_INFO("Mythos:");
-    MYTHOS_INFO("\t- Version: {}", MYTHOS_VERSION_STRING);
+    MYTHOS_INFO("\t- Version: {} ({})", MYTHOS_VERSION_STRING, MYTHOS_VERSION);
 
     auto app = myth::create_application();
     app->run();
