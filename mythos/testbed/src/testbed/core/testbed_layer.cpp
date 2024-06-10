806c47f1545a5a3e049321949044863a6dcb5833
WarEagle451 <56392435+WarEagle451@users.noreply.github.com>
2024-06-09 23:43:45 -0400

    Clear color RGB

--- mythos/testbed/src/testbed/core/testbed_layer.cpp
+++ mythos/testbed/src/testbed/core/testbed_layer.cpp
@@ -7,6 +7,8 @@
 
 #include <format>
 
+#include <myl_wip/color.hpp>
+
 namespace testbed {
     MYL_NO_DISCARD testbed_layer::testbed_layer()
         : myth::layer("testbed") {
@@ -30,39 +32,17 @@ namespace testbed {
         d.dispatch<myth::event::key_pressed>(MYTHOS_BIND_EVENT_FUNC(testbed_layer::on_key_pressed));
     }
 
-    myl::f32vec3 clear_color{ 0.f, 0.f, 0.f };
-    myl::u32 clear_color_index = 0;
-    myl::vec3<bool> clear_color_count_up{ true, true, true };
-
+    myl::f32vec3 hsv_clear_color{ 0.f, 1.f, 1.f };
     bool vsync_toggle = true;
 
     auto testbed_layer::update(MYL_MAYBE_UNUSED myth::timestep ts) -> void {
-        if (myth::application::get().main_window()->state() != myth::window_state::minimized) {
-            ///m_smooth_ts = (m_smooth_ts * 0.995f) + (ts * 0.005f); // Update timestep smoothly = (old_timestep * smoothing_factor) + (new_timestep * (1.0 - smoothing_factor))
-            ///myth::application::get().main_window()->set_title(std::format("FPS: {:.2f}", 1.f / m_smooth_ts).c_str());
+        if (myth::application::get().main_window()->state() != myth::window_state::minimized)
             myth::application::get().main_window()->set_title(std::format("FPS: {:.2f}", 1.f / static_cast<float>(ts)).c_str());
-        }
 
-        if (clear_color_count_up[clear_color_index]) { /// There must be a way to make this smoother
-            clear_color[clear_color_index] += 0.005f;
-            if (clear_color[clear_color_index] >= 1.f) {
-                clear_color_count_up[clear_color_index] = !clear_color_count_up[clear_color_index];
-                ++clear_color_index;
-                if (clear_color_index == 3)
-                    clear_color_index = 0;
-            }
-        }
-        else {
-            clear_color[clear_color_index] -= 0.005f;
-            if (clear_color[clear_color_index] <= 0.f) {
-                clear_color_count_up[clear_color_index] = !clear_color_count_up[clear_color_index];
-                ++clear_color_index;
-                if (clear_color_index == 3)
-                    clear_color_index = 0;
-            }
-        }
-
-        myth::renderer::set_clear_color(clear_color);
+        hsv_clear_color[0] += 100.f * ts;
+        if (hsv_clear_color[0] >= 360.f)
+            hsv_clear_color[0] -= 360.f;
+        myth::renderer::set_clear_color(myl::color::hsv_to_rgb(hsv_clear_color));
     }
 
     auto testbed_layer::render() -> void {
